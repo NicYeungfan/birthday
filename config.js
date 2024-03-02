7724c29091a4e7b58e3189750167b0325e7e3154
NicYeungfan <103891436+NicYeungfan@users.noreply.github.com>
2024-03-02 16:50:28 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
-// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+// 2024/03/02 https://github.com/AJLoveChina/birthday
 var config = {
-    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
-    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    // Memory to Nic&&Vicky (杨帆&&毛倩)
+    // 
     texts: [
-        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的小可爱",  // 同上...
-        "今天是你的生日",
-        "这是我们在一起的",
-        "第三个生日了哦",
-        "去年的生日",
-        "还记得在王婆串串吃的呢",
-        "今年要吃好的喽哦",
-        "要把我家可爱猪猪喂饱饱",
-        "然后抱走",
-        "YAMI~~",
+        "TO",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "MY DEAR",  // 同上...
+        "LOVE",
+        "WE",
+        "WILL",
+        "BE",
+        "HAPPY",
+        "YOU",
+        "WILL",
+        "BE",
+        "LUCKY~~",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -25,8 +25,8 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
-        // "今天是你的生日": "./imgs/birthday.jpg",
+        "LOVE": "./imgs/xiaokeai.png",
+        // "LUCKY": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
@@ -36,7 +36,7 @@ var config = {
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        wish_message: "YOU&&ME",
         story: "A MESSAGE FOR YOU",
     }
 };
